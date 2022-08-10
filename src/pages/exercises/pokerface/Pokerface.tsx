69d57418dc916e527bfef0ff4af2bfca3e90a7c7
Kieran Magee <kieran.magee@instil.co>
2022-08-10 11:51:53 +0100

    fixing structure

--- src/pages/exercises/pokerface/Pokerface.tsx
+++ src/pages/exercises/pokerface/Pokerface.tsx
@@ -21,22 +21,22 @@ export const PokerHandsExercise: FC = () => {
     }
 
     return (
-    <>
-        <Navbar pageTitle="Pokerface Exercise"/>
-        <div className="pokerface-exercise">
-            <div className="pokerface-exercise__task">
-                <h3 className="pokerface-exercise__title">Task:</h3>
-                <p className="pokerface-exercise__description">Evaluate the following pokerhands</p>
-                <textarea cols={60} rows={6} value={input} onChange={e => setInput(e.target.value)}></textarea>
+        <>
+            <Navbar pageTitle="Pokerface Exercise"/>
+            <div className="pokerface-exercise">
+                <div className="pokerface-exercise__task">
+                    <h3 className="pokerface-exercise__title">Task:</h3>
+                    <p className="pokerface-exercise__description">Evaluate the following pokerhands</p>
+                    <textarea cols={60} rows={6} value={input} onChange={e => setInput(e.target.value)}></textarea>
+                </div>
+                <button className="process-button" onClick={execute}>Process</button>
+                <h3 className="pokerface-exercise__sub-title">Output:</h3>
+                <p className="pokerface-exercise__results">
+                    {!output && <TypingCaret/>}
+                    <p>{output}</p>
+                </p>
             </div>
-            <button className="process-button" onClick={execute}>Process</button>
-            <h3 className="pokerface-exercise__sub-title">Output:</h3>
-            <p className="pokerface-exercise__results">
-                {!output && <TypingCaret/>}
-                <p>{output}</p>
-            </p>
-        </div>
-    </>
+        </>
     )
 };
 
