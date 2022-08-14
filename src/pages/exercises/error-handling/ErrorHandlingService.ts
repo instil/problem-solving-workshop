d5e3c5b5879f08bd0ff33c4f70527ac0fa117e47
Kieran Magee <kieran.magee@instil.co>
2022-08-14 23:13:40 +0100

    Adding answers

--- src/pages/exercises/error-handling/ErrorHandlingService.ts
+++ src/pages/exercises/error-handling/ErrorHandlingService.ts
@@ -1,15 +1,27 @@
 import {NameError} from "./errors/NameError";
 
+function isValidName(value: string): boolean {
+    if (!/^[a-zA-Z]+$/.test(value)) {
+        throw new NameError("Your name should contain only letters");
+    }
+    return true;
+}
+
 export function evaluateName(value: string): string {
     // TODO: fix this code and add appropriate errors & logs
     try {
-        if (!/^[a-zA-Z]+$/.test(value)) {
-           // do something
+        value = value.trim();
+        if (value === "") {
+            throw new NameError("Name is required!");
+        }
+        if (!isValidName(value)) {
+            throw new NameError("Your name should contain only letters");
         }
 
         return value;
     } catch (error) {
-        // do something
-        return value;
+        // eslint-disable-line no-console
+        console.error("An error has occurred", error);
+        throw new NameError(`Unable to evaluate name, ${value}`);
     }
 }
--- src/pages/exercises/pokerface/PokerHand.ts
+++ src/pages/exercises/pokerface/PokerHand.ts
@@ -1,7 +1,42 @@
-// TODO: Solutions goes here
-//       Return a string of the name of the poker hand
-//       https://en.wikipedia.org/wiki/List_of_poker_hands
-export function calculatePokerHand(hand: string) {
+import _ from "lodash";
 
-    return "Unknown";
+const allRanksAceLow = "A23456789TJQK";
+const allRanksAceHigh = "23456789TJQKA";
+
+function uniqueCount(items: string[]): number {
+    return new Set(items).size;
+}
+
+export function calculatePokerHand(hand: string) {
+    const cards = hand.split(" ").map(pair => ({rank: pair[0], suit: pair[1]}));
+    const ranks = cards.map(x => x.rank);
+    const suits = cards.map(x => x.suit);
+
+    const getSortedRanks = (scheme: string) => _.sortBy(ranks, x => scheme.indexOf(x)).join("");
+    const getRankCounts = () => _.chain(cards)
+        .groupBy(x => x.rank)
+        .map(cards => cards.length)
+        .value();
+
+    const isRoyalFlush = () => isStraightFlush() && ranks.includes("K") && ranks.includes("A");
+    const isStraightFlush = () => isStraight() && isFlush();
+    const isStraight = () => isStraightIn(allRanksAceLow) || isStraightIn(allRanksAceHigh);
+    const isStraightIn = (scheme: string) => scheme.includes(getSortedRanks(scheme));
+    const isFlush = () => uniqueCount(suits) === 1;
+    const isFullHouse = () => uniqueCount(ranks) === 2;
+    const isThreeOfAKind = () => Math.max(...getRankCounts()) === 3;
+    const isFourOfAKind = () => Math.max(...getRankCounts()) === 4;
+    const isTwoPair = () => uniqueCount(ranks) < 4;
+    const isPair = () => uniqueCount(ranks) < 5;
+
+    if (isRoyalFlush()) return "Royal Flush";
+    if (isStraightFlush()) return "Straight Flush";
+    if (isFourOfAKind()) return "Four of a Kind";
+    if (isStraight()) return "Straight";
+    if (isFlush()) return "Flush";
+    if (isFullHouse()) return "Full House";
+    if (isThreeOfAKind()) return "Three of a Kind";
+    if (isTwoPair()) return "Two Pair";
+    if (isPair()) return "Pair";
+    return "High Card";
 }
