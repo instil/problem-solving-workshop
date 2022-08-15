4dae8e5d9cc32873c3976ed1437c52fe212949d5
Kieran Magee <kieran.magee@instil.co>
2022-08-15 22:13:40 +0100

    adding test

--- src/pages/menu/Menu.test.ts
+++ src/pages/menu/Menu.test.ts
@@ -3,4 +3,8 @@ describe("Menu", () => {
     it("should be true", () => {
         expect(true).toBe(true);
     })
+
+    it("should be false", () => {
+        expect(false).toBe(false);
+    })
 });
