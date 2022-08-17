deff54b1f002416f9d0b9a071dcdac7b33c7dc1a
Kieran Magee <kieran.magee@instil.co>
2022-08-17 16:29:53 +0100

    Fixing regex - thanks Peter!!!!!!!

--- src/pages/exercises/error-handling/ErrorHandlingService.ts
+++ src/pages/exercises/error-handling/ErrorHandlingService.ts
@@ -1,27 +1,16 @@
 import {NameError} from "./errors/NameError";
 
 function isValidName(value: string): boolean {
-    if (!/^[a-zA-Z]+$/.test(value)) {
+    if (!/^[a-zA-Z ]+$/.test(value)) {
         throw new NameError("Your name should contain only letters");
     }
     return true;
 }
 
 export function evaluateName(value: string): string {
-    // TODO: fix this code and add appropriate errors & logs
-    try {
-        value = value.trim();
-        if (value === "") {
-            throw new NameError("Name is required!");
-        }
-        if (!isValidName(value)) {
-            throw new NameError("Your name should contain only letters");
-        }
+    value = value.trim();
+    if (value === "") throw new NameError("Name is required!");
+    if (!isValidName(value)) throw new NameError("Your name should contain only letters");
 
-        return value;
-    } catch (error) {
-        // eslint-disable-line no-console
-        console.error("An error has occurred", error);
-        throw new NameError(`Unable to evaluate name, ${value}`);
-    }
+    return value;
 }
