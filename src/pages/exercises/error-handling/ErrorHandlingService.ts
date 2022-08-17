361b199f0a60cac98a91b31a80ffcd4afa37087d
Daniel Murphy <daniel.murphy@instil.co>
2022-08-17 15:57:56 +0100

    adding error handling

--- src/pages/exercises/error-handling/ErrorHandlingService.ts
+++ src/pages/exercises/error-handling/ErrorHandlingService.ts
@@ -1,15 +1,14 @@
 import {NameError} from "./errors/NameError";
 
 export function evaluateName(value: string): string {
-    // TODO: fix this code and add appropriate errors & logs
     try {
         if (!/^[a-zA-Z]+$/.test(value)) {
-           // do something
+           throw new Error(`invalid name: ${value}`);
         }
 
-        return value;
+        return `Hello, ${value}`;
     } catch (error) {
-        // do something
-        return value;
+        console.error(error);
+        return "";
     }
 }
